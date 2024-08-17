aa22875fcf5e820305a25b08c5c8602c5fc31c01
Parigi Sai Teja Reddy <93255361+ParigiSaiTeja@users.noreply.github.com>
2024-08-16 20:35:51 -0500

    Yayy

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -24,6 +24,11 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
           mt-10
           grid 
           grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
           gap-8
           mb-4
         "
